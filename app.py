b23d9939554444c53d79feb0db765b9751e035dc
mekb the turtle <mekbgithub@icloud.com>
2022-10-31 11:08:10 +1100

    fix error

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ if len(sys.argv) != 2 or sys.argv[1] == "-help" or sys.argv[1] == "-h":
 device_found = False
 usb_backend  = fusee.HaxBackend.create_appropriate_backend()
 
-def build_mountusb_path(self):
+def build_mountusb_path():
     try:
         path = sys._MEIPASS
     except Exception:
         path = os.path.abspath('.')
     return os.path.join(path, 'memloader.bin')
 
-def build_relocator_path(self):
+def build_relocator_path():
     try:
         path = sys._MEIPASS
     except Exception:
@@ -38,7 +38,7 @@ if sys.argv[1] == "-sdmount":
     args.payload   = build_mountusb_path()
 else:
     print("Found device, sending payload...")
-    args.payload   = payload_path
+    args.payload   = sys.argv[1]
 args.relocator = build_relocator_path()
 fusee.do_hax(args)
 
